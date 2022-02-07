42f8d21ed3b6565754863f864717c128ec8a3944
ttasjwi <ttasjwi920@gmail.com>
2022-02-07 14:29:38 +0900

    Initial commit

---
+++ src/main/java/com/ttasjwi/PCRoom/PcRoomApplication.java
@@ -0,0 +1,13 @@
+package com.ttasjwi.PCRoom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcRoomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ttasjwi/PCRoom/PcRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttasjwi.PCRoom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcRoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
